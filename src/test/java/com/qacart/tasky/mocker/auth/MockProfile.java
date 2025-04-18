d3bd86faad6a4e5302095885d82a59d2163b5e58
Tamer Alsharawneh <tameralsharawneh12@gmail.com>
2025-04-18 22:51:00 +0300

    prepare mocking for subscription test cases

---
+++ src/test/java/com/qacart/tasky/mocker/auth/MockProfile.java
@@ -0,0 +1,29 @@
+package com.qacart.tasky.mocker.auth;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public final class MockProfile {
+    private MockProfile() {}
+    private static final String testUrl = "/api/auth/profile";
+    public static void mockSubscribedProfile() {
+        String mockedFile = "subscribedProfile.json";
+        stubFor(get(urlEqualTo(testUrl))
+                .willReturn(
+                        aResponse()
+                                .withBodyFile(mockedFile)
+                                .withStatus(200)
+                )
+        );
+    }
+
+    public static void mockCanceledProfile() {
+        String mockedFile = "InactiveProfile.json";
+        stubFor(get(urlEqualTo(testUrl))
+                .willReturn(
+                        aResponse()
+                                .withBodyFile(mockedFile)
+                                .withStatus(200)
+                )
+        );
+    }
+}
---
+++ src/test/java/com/qacart/tasky/mocker/subscription/MockSubscriptions.java
@@ -0,0 +1,49 @@
+package com.qacart.tasky.mocker.subscription;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+public final class MockSubscriptions {
+    private MockSubscriptions() {}
+
+    public static void mockActiveSubscription() {
+        String testUrl = "/api/subscriptions";
+        String mockedFile = "activatedSubscription.json";
+        stubFor(
+                post(urlEqualTo(testUrl))
+                .willReturn(
+                        aResponse()
+                                .withStatus(200)
+                                .withBodyFile(mockedFile)
+                )
+        );
+    }
+
+    public static void mockCloseToSubscriptionExpiration() {
+        String testUrl = "/api/subscriptions/current";
+        String mockedFile = "expiredSubscription.json";
+        stubFor(get(urlEqualTo(testUrl))
+                .willReturn(
+                        aResponse()
+                                .withStatus(200)
+                                .withBodyFile(mockedFile)
+                )
+        );
+    }
+
+    public static void mockCurrentSubscriptionToBeActive() {
+        String testUrl = "/api/subscriptions/current";
+        String mockedFile = "currentCloseToExpiredSubscription.json";
+        stubFor(get(urlEqualTo(testUrl))
+                .willReturn(
+                        aResponse()
+                        .withStatus(200)
+                        .withBodyFile(mockedFile)
+                )
+        );
+    }
+
+    public static void mockCancelledSubscription() {
+        String testUrl = "/api/subscriptions/cancel";
+        String mockedFile = "cancelledSubscription.json";
+    }
+}
